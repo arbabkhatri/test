f09ca0e91ad960b23b43a2b81c0293b610fa2d56
arbabkhatri <77720991+arbabkhatri@users.noreply.github.com>
2026-02-11 01:49:45 +0500

    Create src/NewsAnalysis.tsx

---
+++ src/NewsAnalysis.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NewsAnalysis() {
+  return (
+    <div>
+      <h2>Metallic Market News Analysis</h2>
+      <p>This section will provide news and analysis related to the metallic market.</p>
+      {/* Add news and analysis content here */}
+    </div>
+  );
+}
+
+export default NewsAnalysis;
